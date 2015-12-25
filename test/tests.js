f9f6f1d02be4b642aca2e213e12370f6045d1180
James M. Greene <james.m.greene@gmail.com>
2015-12-24 22:49:32 -0600

    Add '_hasAttribute' helper method to tests to resolve IE7 failures

--- test/tests.js
+++ test/tests.js
@@ -26,6 +26,23 @@
   }
 
 
+  // IE<8 does not have the `HTMLElement.prototype.hasAttribute` method
+  function _hasAttribute(node, attrName) {
+    var mayHaveAttr = (
+      node &&
+      node.nodeType === 1 &&
+      typeof node[attrName] !== "undefined"
+    );
+    if (mayHaveAttr) {
+      if (node.hasAttribute) {
+        return node.hasAttribute(attrName);
+      }
+      return node.getAttribute(attrName) === node[attrName];
+    }
+    return false;
+  }
+
+
   module("`_currentEvaluatingScript`");
 
   test("Is defined", function(assert) {
@@ -36,79 +53,12 @@
 
   module("`_currentEvaluatingScript()` follows the spec");
 
-  (canPolyfill ? test : skip)("1 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(1);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-  });
-
-  (canPolyfill ? test : skip)("2 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(2);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-    var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-    assert.strictEqual(polyfillResultNodeName, "SCRIPT", "`_currentEvaluatingScript()` should return a `script` node");
-  });
-
-  (canPolyfill ? test : skip)("3 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(3);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-    var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-    var polyfillResultHasSrcAttr = polyfillResultIsNonNullObject && !!polyfillResultSync.hasAttribute ? polyfillResultSync.hasAttribute("src") : false;
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-    assert.strictEqual(polyfillResultNodeName, "SCRIPT", "`_currentEvaluatingScript()` should return a `script` node");
-    assert.strictEqual(polyfillResultHasSrcAttr, true, "`_currentEvaluatingScript()` should return a `script` node with a `src` attribute");
-  });
-
-  (canPolyfill ? test : skip)("3.1 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(3);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-    var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-    assert.strictEqual(polyfillResultNodeName, "SCRIPT", "`_currentEvaluatingScript()` should return a `script` node");
-    assert.strictEqual(polyfillResultSync.outerHTML, "<script></script>", "`_currentEvaluatingScript()` should return a `script` node with some outer HTML");
-  });
-
-  (canPolyfill ? test : skip)("3.2 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(3);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-    var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-    assert.strictEqual(polyfillResultNodeName, "SCRIPT", "`_currentEvaluatingScript()` should return a `script` node");
-    assert.strictEqual(polyfillResultSync.text, "Script inner text", "`_currentEvaluatingScript()` should return a `script` node with some inner text");
-  });
-
-  (canPolyfill ? test : skip)("4 - When invoked during a script's synchronous evaluation", function(assert) {
-    assert.expect(4);
-
-    var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
-    var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-    var polyfillResultHasSrcAttr = polyfillResultIsNonNullObject && !!polyfillResultSync.hasAttribute ? polyfillResultSync.hasAttribute("src") : false;
-    var polyfillResultSrc = polyfillResultIsNonNullObject ? (polyfillResultHasSrcAttr ? polyfillResultSync.src : null) : undefined;
-    var polyfillResultSrcSuffix = polyfillResultSrc ? polyfillResultSrc.replace(/\?.*$/, "").slice(-9).toLowerCase() : polyfillResultSrc;
-
-    assert.strictEqual(polyfillResultIsNonNullObject, true, "`_currentEvaluatingScript()` should return a non-`null` object");
-    assert.strictEqual(polyfillResultNodeName, "SCRIPT", "`_currentEvaluatingScript()` should return a `script` node");
-    assert.strictEqual(polyfillResultHasSrcAttr, true, "`_currentEvaluatingScript()` should return a `script` node with a `src` attribute");
-    assert.strictEqual(polyfillResultSrcSuffix, "/tests.js", "`_currentEvaluatingScript().src` should return the test script's path");
-  });
-
   (canPolyfill ? test : skip)("When invoked during a script's synchronous evaluation", function(assert) {
     assert.expect(4);
 
     var polyfillResultIsNonNullObject = polyfillResultSync != null && typeof polyfillResultSync === "object";
     var polyfillResultNodeName = polyfillResultIsNonNullObject ? polyfillResultSync.nodeName : undefined;
-    var polyfillResultHasSrcAttr = polyfillResultIsNonNullObject && !!polyfillResultSync.hasAttribute ? polyfillResultSync.hasAttribute("src") : false;
+    var polyfillResultHasSrcAttr = polyfillResultIsNonNullObject ? _hasAttribute(polyfillResultSync, "src") : false;
     var polyfillResultSrc = polyfillResultIsNonNullObject ? (polyfillResultHasSrcAttr ? polyfillResultSync.src : null) : undefined;
     var polyfillResultSrcSuffix = polyfillResultSrc ? polyfillResultSrc.replace(/\?.*$/, "").slice(-9).toLowerCase() : polyfillResultSrc;
 
@@ -140,7 +90,7 @@
 
     var nativeResultIsNonNullObject = nativeResultSync != null && typeof nativeResultSync === "object";
     var nativeResultNodeName = nativeResultIsNonNullObject ? nativeResultSync.nodeName : undefined;
-    var nativeResultHasSrcAttr = nativeResultIsNonNullObject && !!nativeResultSync.hasAttribute ? nativeResultSync.hasAttribute("src") : false;
+    var nativeResultHasSrcAttr = nativeResultIsNonNullObject ? _hasAttribute(nativeResultSync, "src") : false;
     var nativeResultSrc = nativeResultIsNonNullObject ? (nativeResultHasSrcAttr ? nativeResultSync.src : null) : undefined;
     var nativeResultSrcSuffix = nativeResultSrc ? nativeResultSrc.replace(/\?.*$/, "").slice(-9).toLowerCase() : nativeResultSrc;
 
